d8e0d6c8e051237d0a29fb59ac708dfa6a3bcc72
Sohee <sohee3480@gmail.com>
2021-09-02 16:37:14 +0900

    변수 설정

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -2,8 +2,17 @@ package com.example.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private val heightEdt = findViewById<EditText>(R.id.heightEdt)
+    private val weightEdt = findViewById<EditText>(R.id.weightEdt)
+    private val womanBtn = findViewById<Button>(R.id.womanBtn)
+    private val manBtn = findViewById<Button>(R.id.manBtn)
+    private val bmiBtn = findViewById<Button>(R.id.bmiCheck)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
