ee7129e1456652096cba6ffda6f1622642340fce
Sohee <sohee3480@gmail.com>
2021-09-06 14:09:01 +0900

    성별에 따른 정상체중

--- app/src/main/java/com/example/bmi/ResultActivity.kt
+++ app/src/main/java/com/example/bmi/ResultActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.core.graphics.blue
 import androidx.core.graphics.toColorInt
+import java.lang.reflect.Array.get
 import kotlin.math.pow
 
 class ResultActivity : AppCompatActivity() {
@@ -14,8 +15,9 @@ class ResultActivity : AppCompatActivity() {
 
         val height = intent.getIntExtra("height", 0)
         val weight = intent.getIntExtra("weight", 0)
+        val wm = intent.getStringExtra("WM")
 
-        val bmi = weight / ( height * height )
+        val bmi = weight / (height / 100.0).pow(2.0)
         val result = when {
             bmi >= 35.0 -> "고도 비만"
             bmi >= 30.0 -> "중경도 비만"
@@ -25,7 +27,12 @@ class ResultActivity : AppCompatActivity() {
             else -> "저체중"
         }
 
-        val standardWeight = (height - 100) * 0.9
+        val standardWeight: Double =
+        when (wm) {
+            "M" -> (height * height) * 22.0 * 0.0001
+            "W" -> height.toDouble().pow(2) * 21 * 0.0001
+            else -> 0.0
+        }
 
         val bmiTxt: TextView by lazy { findViewById(R.id.bmi) }
         val resultTxt: TextView by lazy { findViewById(R.id.result) }
